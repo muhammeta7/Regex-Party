ab943a25c3134252f8d1e6dc0003e1375a9b3c4d
muhammeta7 <muhammeta7@gmail.com>
2020-05-13 18:50:40 -0400

    completed question for frown count

--- regex-app/src/main/java/com/regex/app/MediumRegex.java
+++ regex-app/src/main/java/com/regex/app/MediumRegex.java
@@ -41,4 +41,27 @@ public class MediumRegex {
         return counter;
     }
 
+    // Write a program to count the total number of frowny smileys in a string.
+    // Here are the rules for how to find smileys and determine whether they are happy or frowny:
+    // Optionally, a smiley may start with eyebrows. Eyebrows are marked with the character >.
+    // Each smiley must have a pair of eyes. The characters used for eyes are :, ;, or 8.
+    // Using only eyes, it's difficult to determine whether a smiley is happy or not. So you need to proceed further with the analysis.
+    // Optionally, a smiley may cry. Any tears must follow the eyes and are marked with the character '.
+    // But be careful, they could be tears of happiness.
+    // Optionally, a smiley may have a nose marked with -, o or ~.
+    // The last and most evident sign of the smiley's mood is its mouth.
+    // A frowny smiley has a mouth marked with one of these characters: (, C, <, [, {, or @.
+    // The distinguishing feature of a happy smiley face is a mouth marked with ), ], }, D, or 3.
+    // Each smiley starts with eyebrows or eyes and ends with the mouth.
+    public int printFrownCount(String input){
+        int counter = 0;
+        String[] word = input.split(" ");
+        for(String s : word){
+            if(s.matches(".*[@<C{\\[ \\\\( ].*")){
+                counter++;
+            }
+        }
+        return counter;
+    }
+
 }
